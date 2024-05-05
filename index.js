d6fdc9c9b617f1a49c4deadc9a453fef8262c782
Hannes Alexandersson <S103154@gmail.com>
2024-05-05 17:38:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,20 +12,21 @@ const port = PORT;
 
 
 //cross origin resource sharing settings to make the API work
-const corsOptions = {
+/* const corsOptions = {
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   };
   
-server.use(cors(corsOptions));
+server.use(cors(corsOptions)); */
+server.use(cors());
 
 server.use(express.json({limit: '50mb'}));
 server.use(express.urlencoded({limit: '50mb'}))
 server.use(express.json());
-server.use(bodyParser.urlencoded({ extended: false }));
-server.use(bodyParser.json())
+/* server.use(bodyParser.urlencoded({ extended: false }));
+server.use(bodyParser.json()) */
 
 server.use('/api/system/', systemRoutes);
 
