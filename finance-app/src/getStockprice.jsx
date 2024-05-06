31bf7adc1d760ba7b4b1b414d27be83bf9503413
Hannes Alexandersson <S103154@gmail.com>
2024-05-06 11:51:39 +0200

    Update getStockprice.jsx

--- finance-app/src/getStockprice.jsx
+++ finance-app/src/getStockprice.jsx
@@ -1,19 +1,18 @@
-  //https://hannes-restapi-ff3b89ded8f9.herokuapp.com/api/system/getStockPrice
+function get_stock_price( stockName ){
+    //https://hannes-restapi-ff3b89ded8f9.herokuapp.com/api/system/getStockPrice
     //https://restapi-two-pi.vercel.app/api/system/getStockPrice
     //http://localhost:3000/api/system/getStockPrice
     //https://finance-app-five-neon.vercel.app/api/system/getStockPrice
     //https://restapi-9k9x.onrender.com/api/system/getStockPrice/
-
-function get_stock_price( stockName ){
-  
-    const url = `https://restapi-9k9x.onrender.com/api/system/getStockPrice/${stockName}`;    
+    const url = 'https://restapi-9k9x.onrender.com/api/system/getStockPrice';    
    
     return fetch(url, { 
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',     
            
-        }
+        },       
+        body: JSON.stringify({ stockName })
     })
     .then(response => {
         if (response.ok) {
